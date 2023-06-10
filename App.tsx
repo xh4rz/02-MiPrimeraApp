513e9fe5f8280e73b44dc273dc9bf76db616663a
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2023-06-10 06:07:19 -0500

    51. Crear pantallas independientes

--- App.tsx
+++ App.tsx
@@ -1,12 +1,6 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { HolaMundoScreen } from './src/screens/HolaMundoScreen';
 
-const App = () => {
-	return (
-		<View style={{ flex: 1, backgroundColor: 'red', justifyContent: 'center' }}>
-			<Text style={{ fontSize: 45, textAlign: 'center' }}>Hola Mundo</Text>
-		</View>
-	);
+export const App = () => {
+	return <HolaMundoScreen />;
 };
-
-export default App;
