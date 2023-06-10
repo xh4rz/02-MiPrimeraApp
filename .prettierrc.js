c5d95b8acf3eab48035ea06b71b065e07463f99f
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2023-06-10 05:59:48 -0500

    50. Hola Mundo

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,8 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
+  // arrowParens: 'avoid',
+  // bracketSameLine: true,
+  // bracketSpacing: false,
   singleQuote: true,
-  trailingComma: 'all',
+  trailingComma: 'none',
+  useTabs: true
 };
