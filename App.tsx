6900090fd3e943f3e099fc68271031dbba66b961
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2023-09-07 03:03:42 -0500

    69. Height, Width porcentual y dimensiones de la pantalla

--- App.tsx
+++ App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 // import { HolaMundoScreen } from './src/screens/HolaMundoScreen';
 // import { ContadorScreen } from './src/screens/ContadorScreen';
-import { BoxObjectModelScreen } from './src/screens/BoxObjectModelScreen';
+// import { BoxObjectModelScreen } from './src/screens/BoxObjectModelScreen';
 import { SafeAreaView } from 'react-native';
+import { DimensionesScreen } from './src/screens/DimensionesScreen';
 
 export const App = () => {
 	return (
 		<SafeAreaView style={{ flex: 1 }}>
 			{/* <HolaMundoScreen />  */}
 			{/* <ContadorScreen />   */}
-			<BoxObjectModelScreen />
+			{/* <BoxObjectModelScreen /> */}
+			<DimensionesScreen />
 		</SafeAreaView>
 	);
 };
