4bbec575d33d8936b8b8f713e5681cd7ee4fce19
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-06-18 02:29:03 -0500

    76. Resolución de la tarea de diseños

--- src/screens/HomeworkScreen.tsx
+++ src/screens/HomeworkScreen.tsx
@@ -25,9 +25,232 @@ const styles = StyleSheet.create({
 		backgroundColor: '#5856D6'
 	},
 	orangeBox: {
-		backgroundColor: '#F0A23B'
+		backgroundColor: '#F0A23B',
+		flex: 1
 	},
 	blueBox: {
 		backgroundColor: '#28C4D9'
 	}
 });
+
+// const styles = StyleSheet.create({
+// 	container: {
+// 		flex: 1,
+// 		backgroundColor: '#28425B',
+// 		justifyContent: 'center'
+// 	},
+// 	box: {
+// 		width: 100,
+// 		height: 100,
+// 		borderWidth: 10,
+// 		borderColor: 'white'
+// 	},
+// 	purpleBox: {
+// 		backgroundColor: '#5856D6'
+// 	},
+// 	orangeBox: {
+// 		backgroundColor: '#F0A23B'
+// 	},
+// 	blueBox: {
+// 		backgroundColor: '#28C4D9',
+// 		width: '100%'
+// 	}
+// });
+
+// const styles = StyleSheet.create({
+// 	container: {
+// 		flex: 1,
+// 		backgroundColor: '#28425B',
+// 		justifyContent: 'center'
+// 	},
+// 	box: {
+// 		width: 100,
+// 		height: 100,
+// 		borderWidth: 10,
+// 		borderColor: 'white'
+// 	},
+// 	purpleBox: {
+// 		backgroundColor: '#5856D6',
+// 		alignSelf: 'flex-end'
+// 	},
+// 	orangeBox: {
+// 		backgroundColor: '#F0A23B'
+// 	},
+// 	blueBox: {
+// 		backgroundColor: '#28C4D9',
+// 		alignSelf: 'center'
+// 	}
+// });
+
+// const styles = StyleSheet.create({
+// 	container: {
+// 		flex: 1,
+// 		backgroundColor: '#28425B',
+// 		justifyContent: 'space-between',
+// 		alignItems: 'center'
+// 	},
+// 	box: {
+// 		width: 100,
+// 		height: 100,
+// 		borderWidth: 10,
+// 		borderColor: 'white'
+// 	},
+// 	purpleBox: {
+// 		backgroundColor: '#5856D6',
+// 		alignSelf: 'flex-end'
+// 	},
+// 	orangeBox: {
+// 		backgroundColor: '#F0A23B'
+// 	},
+// 	blueBox: {
+// 		backgroundColor: '#28C4D9',
+// 		alignSelf: 'flex-start'
+// 	}
+// });
+
+// const styles = StyleSheet.create({
+// 	container: {
+// 		flex: 1,
+// 		backgroundColor: '#28425B',
+// 		justifyContent: 'space-between',
+// 		flexDirection: 'row'
+// 	},
+// 	box: {
+// 		width: 100,
+// 		borderWidth: 10,
+// 		borderColor: 'white'
+// 	},
+// 	purpleBox: {
+// 		backgroundColor: '#5856D6'
+// 	},
+// 	orangeBox: {
+// 		backgroundColor: '#F0A23B'
+// 	},
+// 	blueBox: {
+// 		backgroundColor: '#28C4D9'
+// 	}
+// });
+
+// const styles = StyleSheet.create({
+// 	container: {
+// 		flex: 1,
+// 		backgroundColor: '#28425B'
+// 	},
+// 	box: {
+// 		borderWidth: 10,
+// 		borderColor: 'white'
+// 	},
+// 	purpleBox: {
+// 		backgroundColor: '#5856D6',
+// 		flex: 2
+// 	},
+// 	orangeBox: {
+// 		backgroundColor: '#F0A23B',
+// 		flex: 2
+// 	},
+// 	blueBox: {
+// 		backgroundColor: '#28C4D9',
+// 		flex: 4
+// 	}
+// });
+
+// const styles = StyleSheet.create({
+// 	container: {
+// 		flex: 1,
+// 		backgroundColor: '#28425B',
+// 		justifyContent: 'center',
+// 		alignItems: 'center'
+// 	},
+// 	box: {
+// 		width: 100,
+// 		height: 100,
+// 		borderWidth: 10,
+// 		borderColor: 'white'
+// 	},
+// 	purpleBox: {
+// 		backgroundColor: '#5856D6'
+// 	},
+// 	orangeBox: {
+// 		backgroundColor: '#F0A23B'
+// 	},
+// 	blueBox: {
+// 		backgroundColor: '#28C4D9'
+// 	}
+// });
+
+// const styles = StyleSheet.create({
+// 	container: {
+// 		flex: 1,
+// 		backgroundColor: '#28425B',
+// 		justifyContent: 'center',
+// 		alignItems: 'center'
+// 	},
+// 	box: {
+// 		width: 100,
+// 		height: 100,
+// 		borderWidth: 10,
+// 		borderColor: 'white'
+// 	},
+// 	purpleBox: {
+// 		backgroundColor: '#5856D6'
+// 	},
+// 	orangeBox: {
+// 		backgroundColor: '#F0A23B',
+// 		right: -100
+// 	},
+// 	blueBox: {
+// 		backgroundColor: '#28C4D9'
+// 	}
+// });
+
+// const styles = StyleSheet.create({
+// 	container: {
+// 		flex: 1,
+// 		backgroundColor: '#28425B',
+// 		justifyContent: 'center',
+// 		alignItems: 'center'
+// 	},
+// 	box: {
+// 		width: 100,
+// 		height: 100,
+// 		borderWidth: 10,
+// 		borderColor: 'white'
+// 	},
+// 	purpleBox: {
+// 		backgroundColor: '#5856D6',
+// 		top: 100
+// 	},
+// 	orangeBox: {
+// 		backgroundColor: '#F0A23B',
+// 		right: -100
+// 	},
+// 	blueBox: {
+// 		backgroundColor: '#28C4D9'
+// 	}
+// });
+
+// const styles = StyleSheet.create({
+// 	container: {
+// 		flex: 1,
+// 		backgroundColor: '#28425B',
+// 		justifyContent: 'center',
+// 		alignItems: 'center',
+// 		flexDirection: 'row'
+// 	},
+// 	box: {
+// 		width: 100,
+// 		height: 100,
+// 		borderWidth: 10,
+// 		borderColor: 'white'
+// 	},
+// 	purpleBox: {
+// 		backgroundColor: '#5856D6'
+// 	},
+// 	orangeBox: {
+// 		backgroundColor: '#F0A23B',
+// 		top: 50
+// 	},
+// 	blueBox: {
+// 		backgroundColor: '#28C4D9'
+// 	}
+// });
