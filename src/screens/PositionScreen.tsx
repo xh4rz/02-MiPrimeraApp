6f59002cc04e938313296d81e1d2e607bb94eda9
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-06-18 00:15:50 -0500

    68. Posición absoluta

--- src/screens/PositionScreen.tsx
+++ src/screens/PositionScreen.tsx
@@ -4,6 +4,7 @@ import { StyleSheet, View } from 'react-native';
 export const PositionScreen = () => {
 	return (
 		<View style={styles.container}>
+			<View style={styles.greenBox} />
 			<View style={styles.purpleBox} />
 			<View style={styles.orangeBox} />
 		</View>
@@ -13,9 +14,9 @@ export const PositionScreen = () => {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: '#28C4D9',
-		justifyContent: 'center',
-		alignItems: 'center'
+		backgroundColor: '#28C4D9'
+		// justifyContent: 'center',
+		// alignItems: 'center'
 	},
 	purpleBox: {
 		width: 100,
@@ -23,8 +24,8 @@ const styles = StyleSheet.create({
 		backgroundColor: '#5856D6',
 		borderWidth: 10,
 		borderColor: 'white',
-		position: 'relative',
-		top: 0
+		position: 'absolute',
+		bottom: 0
 	},
 	orangeBox: {
 		width: 100,
@@ -32,7 +33,20 @@ const styles = StyleSheet.create({
 		backgroundColor: '#F0A23B',
 		borderWidth: 10,
 		borderColor: 'white',
-		top: 0,
-		left: 0
+		position: 'absolute',
+		right: 0
+	},
+	greenBox: {
+		// width: 100,
+		// height: 100,
+		backgroundColor: 'green',
+		borderWidth: 10,
+		borderColor: 'white',
+		// position: 'absolute',
+		// bottom: 0,
+		// right: 0,
+		// top: 0,
+		// left: 0
+		...StyleSheet.absoluteFillObject
 	}
 });
